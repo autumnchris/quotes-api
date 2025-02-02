f5c76b0c27d0a301b4c4e79cc498b3591b15dd0d
autumnchris <autumncbullard@gmail.com>
2025-02-01 20:45:36 -0500

    Update CORS

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 app.use(cors({
-  origin: '*',
+  origin: ['https://autumnchris.github.io', 'http://localhost:8080'],
   methods: [ 'GET' ]
 }));
 
